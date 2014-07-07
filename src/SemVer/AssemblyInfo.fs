b21e68d49cbea419fba3598a97d16559922ac425
kolektiv <andrew@xyncro.com>
2014-07-07 22:49:57 +0100

    add assembly info

---
+++ src/SemVer/AssemblyInfo.fs
@@ -0,0 +1,19 @@
+﻿namespace SemVer
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("SemVer")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("Release")>]
+[<assembly: AssemblyCompany("Xyncro Ltd")>]
+[<assembly: AssemblyProduct("SemVer")>]
+[<assembly: AssemblyCopyright("Copyright © Xyncro Ltd 2014")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyInformationalVersion("1.0.0.0")>]
+[<assembly: ComVisible(false)>]
+[<assembly: Guid("D417A8EB-EFF3-4CEC-97A6-C35C08B804B6")>]
+
+()
